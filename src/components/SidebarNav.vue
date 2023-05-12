3ec1e39b3088c3a2355f1a3b1fef010004f818f1
Hisham Salameh <salamehh@msu.edu>
2023-05-11 21:20:37 -0700

    feat: create sidebar nav

--- src/components/SidebarNav.vue
+++ src/components/SidebarNav.vue
@@ -1,11 +1,27 @@
 <template>
-  <aside>
-    <nav>
-      <ul>
-        <li>Search</li>
-        <li>WatchList</li>
-        <li>News</li>
+  <aside class="h-full w-48 bg-neutral-100 p-6">
+    <div class="mt-4 h-16">
+      <p>-- My Stock App</p>
+    </div>
+    <nav class="flex h-[calc(100%-75px)] flex-col justify-between">
+      <ul class="flex flex-col gap-3">
+        <li class="flex items-center gap-2">
+          <span>☰</span>
+          Dashboard
+        </li>
+        <li class="flex items-center gap-2">
+          <span>☰</span>
+          Watchlist
+        </li>
+        <li class="flex items-center gap-2">
+          <span>☰</span>
+          News
+        </li>
       </ul>
+      <div class="flex items-center gap-2">
+        <span>☰</span>
+        Login
+      </div>
     </nav>
   </aside>
 </template>
