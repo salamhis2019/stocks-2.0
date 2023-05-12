7e6295bdf4b3877db259ca3825896389bc278487
Hisham Salameh <salamehh@msu.edu>
2023-05-12 00:06:57 -0700

    style: add logo and redo verbiage for title

--- src/components/SidebarNav.vue
+++ src/components/SidebarNav.vue
@@ -1,7 +1,8 @@
 <template>
-  <aside class="h-full w-48 bg-neutral-100 p-6">
-    <div class="mt-4 h-16">
-      <p>-- My Stock App</p>
+  <aside class="h-full w-64 bg-neutral-100 p-6">
+    <div class="mb-6 flex items-center gap-2">
+      <img src="../assets/stocks-logo.svg" class="h-8 w-6" alt="" />
+      <h1 class="text-xl font-semibold">Vue Stock App</h1>
     </div>
     <nav class="flex h-[calc(100%-75px)] flex-col justify-between">
       <ul class="flex flex-col gap-3">
