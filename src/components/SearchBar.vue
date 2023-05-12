ecf6b9df1c1bef06916e819dddd01b72349654b9
Hisham Salameh <salamehh@msu.edu>
2023-05-11 21:17:29 -0700

    feat: create search bar and boilerplate data

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import useStockDataStore from "@/stores/stocksData.store.ts";
+
+const { symbol } = useStockDataStore()
+
+const inputText = ref<any>('')
+</script>
+
+<template>
+  <div class="flex h-16 w-full items-center bg-neutral-200 p-6">
+    <input
+      v-model="inputText"
+      type="text"
+      placeholder="Symbol"
+      class="w-1/2 rounded-md border-[0.5px] border-neutral-300 p-2"
+    />
+  </div>
+</template>
