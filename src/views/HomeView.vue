41df29566e1934b37aa919a89acd31a6e1b64aa3
Hisham Salameh <salamehh@msu.edu>
2023-05-11 21:19:52 -0700

    feat: create home view for stock app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <script lang="ts" setup>
 import SidebarNav from "@/components/SidebarNav.vue"
+import SearchBar from "@/components/SearchBar.vue"
+import CardContainer from "@/components/UI/CardContainer.vue";
 </script>
 
 <template>
-  <SidebarNav />
+  <div class="flex h-full w-full">
+    <SidebarNav />
+    <div class="w-full">
+      <SearchBar />
+      <div class="p-6">
+        <h2 class="mb-4 text-xl font-semibold">Price Action</h2>
+        <CardContainer> Hello </CardContainer>
+      </div>
+    </div>
+  </div>
 </template>
