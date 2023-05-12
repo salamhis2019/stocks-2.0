1fd86ef8ec700ccaa6a7064aaf7f97ec99d9d915
Hisham Salameh <salamehh@msu.edu>
2023-05-11 23:57:23 -0700

    feat: create first api call with vantage API

--- src/stores/stocksData.store.ts
+++ src/stores/stocksData.store.ts
@@ -1,13 +1,39 @@
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 interface State {
   symbol: string
+  apiKey: string
+  data: any
 }
 
 export const useProblemsStore = defineStore('stocks', {
   state: (): State => {
     return {
-      symbol: ''
+      symbol: '',
+      apiKey: 'LTSY55G9R1CJFQ11',
+      data: null
+    }
+  },
+  actions: {
+    fetchStockData(ticker: string) {
+      axios
+        .get(
+          `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker.toUpperCase()}&interval=5min&apikey=demo${
+            this.apiKey
+          }`
+        )
+        .then((response) => {
+          console.log(response.data)
+          this.data = response.data
+
+          const firstDataKey = Object.keys(this.data)[0]
+          const metaDataKeys = Object.keys(this.data[firstDataKey])
+          this.symbol = this.data[firstDataKey][metaDataKeys[1]]
+        })
+        .catch((error) => {
+          console.error(error)
+        })
     }
   }
 })
