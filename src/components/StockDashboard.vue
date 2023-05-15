49acc3cfea89604c7f799d5624ee49e6e8beddf1
Hisham Salameh <salamehh@msu.edu>
2023-05-15 00:21:20 -0700

    refactor: rename variables and remove unused code

--- src/components/StockDashboard.vue
+++ src/components/StockDashboard.vue
@@ -6,10 +6,10 @@ import PriceBadge from '@/components/UI/PriceBadge.vue'
 import { computed } from 'vue'
 import { storeToRefs } from 'pinia'
 
-import useStocksDataStore from '@/stores/stocksData.store.ts'
+import useStocksDataStore from '@/stores/stocksData.store'
 
 const stocksDataStore = useStocksDataStore()
-const { dailyData, logoUrl }: any = storeToRefs(stocksDataStore)
+const { dailyData, companyData }: any = storeToRefs(stocksDataStore)
 
 const metaData: any = computed(() => {
   const values = Object.values(dailyData.value)
@@ -17,19 +17,11 @@ const metaData: any = computed(() => {
 })
 
 const dailyTimeSeriesData: any = computed(() => {
-  const values = Object.values(dailyData.value);
+  const values = Object.values(dailyData.value)
   return values[1]
 })
 
-const symbol = computed(() => {
-  if (metaData.value) {
-    const keys = Object.keys(metaData.value);
-    return metaData.value[keys[1]]
-  }
-  return ''
-})
-
-const price = computed(() => {
+const currentPrice = computed(() => {
   if (dailyTimeSeriesData.value) {
     const closingData: any = Object.values(dailyTimeSeriesData.value)[0]
     const closingDataKeys = Object.keys(closingData)
@@ -47,11 +39,13 @@ const previousClosingPrice = computed(() => {
   return ''
 })
 
-const priceChange = computed(() => Number(price.value - previousClosingPrice.value).toFixed(2))
+const priceChange = computed(() =>
+  Number(currentPrice.value - previousClosingPrice.value).toFixed(2)
+)
 
 const date = computed(() => {
   if (metaData.value) {
-    const keys = Object.keys(metaData.value);
+    const keys = Object.keys(metaData.value)
     return metaData.value[keys[2]]
   }
   return ''
@@ -64,15 +58,13 @@ const date = computed(() => {
     <h2 class="mb-4 text-xl font-semibold">Price Action</h2>
     <CardContainer>
       <header class="mb-2 flex items-center gap-2">
-        <img :src="logoUrl" class="h-12" alt="" />
-        <h1 class="text-2xl font-semibold">
-          {{ symbol }}
-        </h1>
+        <img :src="companyData.logo" class="h-12" alt="" />
+        <h1 class="text-2xl font-semibold">{{ companyData.ticker }} - {{ companyData.name }}</h1>
       </header>
       <h2 class="text-neutral-600">Stock and Price Overview</h2>
       <div class="flex items-center gap-2">
         <h2 class="text-3xl font-bold">
-          {{ price }}
+          {{ currentPrice }}
         </h2>
         <PriceBadge :price="priceChange" />
       </div>
