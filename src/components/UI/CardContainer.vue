13f21ef32143bce797e62f7f99696d208001a3d3
Hisham Salameh <salamehh@msu.edu>
2023-05-14 23:39:07 -0700

    style: set height to fit content and center content vertically in card

--- src/components/UI/CardContainer.vue
+++ src/components/UI/CardContainer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="h-48 w-96 rounded-xl border-[0.5px] border-neutral-300 bg-white p-6 shadow-md">
+  <div
+    class="flex h-fit w-80 flex-col justify-center rounded-xl border-[0.5px] border-neutral-300 bg-white p-6 shadow-md"
+  >
     <slot></slot>
   </div>
 </template>
