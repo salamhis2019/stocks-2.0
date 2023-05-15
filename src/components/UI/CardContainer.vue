53d0b44fe787d2b0360f313c33cb4760f068bd9d
Hisham Salameh <salamehh@msu.edu>
2023-05-15 00:25:41 -0700

    style: increase width on card

--- src/components/UI/CardContainer.vue
+++ src/components/UI/CardContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex h-fit w-80 flex-col justify-center rounded-xl border-[0.5px] border-neutral-300 bg-white p-6 shadow-md"
+    class="flex h-fit w-96 flex-col justify-center rounded-xl border-[0.5px] border-neutral-300 bg-white p-6 shadow-md"
   >
     <slot></slot>
   </div>
