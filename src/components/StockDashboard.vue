03d1cfbc3f82e0212ea79c1004cfa175ceb1cfb9
Hisham Salameh <salamehh@msu.edu>
2023-05-15 22:41:47 -0700

    chore: add boilterplate info for today card

--- src/components/StockDashboard.vue
+++ src/components/StockDashboard.vue
@@ -70,20 +70,27 @@ const classes = computed(() => {
   <SearchBar />
   <div class="p-6">
     <h2 class="mb-4 text-xl font-semibold">Price Action</h2>
-    <CardContainer>
-      <header class="mb-2 flex items-center gap-3">
-        <img :src="companyData.logo" class="h-12" alt="" />
-        <h1 class="text-2xl font-semibold">{{ companyData.ticker }} - {{ companyData.name }}</h1>
-      </header>
-      <h2 class="text-neutral-600">Stock and Price Overview</h2>
-      <div class="flex items-center gap-2">
-        <h2 class="my-1 text-3xl font-bold">
-          {{ currentPrice }}
-        </h2>
-        <PriceBadge :price="priceChange" />
-        <p class="text-sm font-medium" :class="classes">({{ percentChange }}%)</p>
-      </div>
-      <p class="text-sm text-neutral-500">{{ date }}</p>
-    </CardContainer>
+    <div class="flex gap-6">
+      <CardContainer>
+        <header class="mb-2 flex items-center gap-3">
+          <img :src="companyData.logo" class="h-12" alt="" />
+          <h1 class="text-2xl font-semibold">{{ companyData.ticker }} - {{ companyData.name }}</h1>
+        </header>
+        <h2 class="text-neutral-600">Stock and Price Overview</h2>
+        <div class="flex items-center gap-2">
+          <h2 class="my-1 text-3xl font-bold">
+            {{ currentPrice }}
+          </h2>
+          <PriceBadge :price="priceChange" />
+          <p class="text-sm font-medium" :class="classes">({{ percentChange }}%)</p>
+        </div>
+        <p class="text-sm text-neutral-500">{{ date }}</p>
+      </CardContainer>
+      <CardContainer>
+        <header>
+          <h2>Today</h2>
+        </header>
+      </CardContainer>
+    </div>
   </div>
 </template>
